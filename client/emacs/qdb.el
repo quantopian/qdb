ce8f034b06718ac1ee3787c849fcf8f90afbee95
Joe Jevnik <joejev@gmail.com>
2014-10-29 13:57:49 -0400

    ENH: Adds (provide 'qdb) to make loading qdb easier

--- client/emacs/qdb.el
+++ client/emacs/qdb.el
@@ -12,6 +12,8 @@
 ;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
+(provide 'run-qdb)
+
 (defvar qdb-executable (executable-find "qdb-cli")
   "Program used by `run-qdb'")
 
