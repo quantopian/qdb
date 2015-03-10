f2a75c068b24ff0243fb9637a0a4fba4a1cf1447
Joe Jevnik <joe@quantopian.com>
2015-03-10 18:31:04 -0400

    BUG: Removes the __future__ statement from comm.py
    This could change the execution context of repl code.

--- qdb/comm.py
+++ qdb/comm.py
@@ -12,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import print_function
-
 from abc import ABCMeta, abstractmethod
 import atexit
 from bdb import Breakpoint
